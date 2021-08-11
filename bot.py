fbab71349950eaf843f44abe0a2acd13d77ab682
Max Friedman <max@discordapp.com>
2021-08-10 17:03:42 -0700

    Message random + use more chat history context

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import re
 import sys
+import threading
 import time
 import traceback
 
@@ -18,25 +19,30 @@ chat_generator.initialize()
 botnick = config.get('irc', 'nick')
 
 count_since_response = 100
-last_respone_time = int(time.time())
+last_response_time = int(time.time())
+
+message_history = {}
 
 def message_handler(username, channel, message, full_user):
+    global message_history
+    if channel not in message_history:
+        message_history[channel] = ""
+    message_history[channel] += message + "\n"
+    message_history[channel] = message_history[channel][-100:]
+
     global count_since_response
     if not should_respond(message):
         count_since_response += 1
         return
 
-    input_str = ""
-    while len(input_str) < 100:
-        input_str += message + "\n"
-
-    reply = chat_generator.generate_reply(input_str)
+    reply = chat_generator.generate_reply(message_history[channel])
     if botnick.upper() in message.upper():
         reply = username + ": " + reply
     irc.send_to_channel(channel, reply)
+    message_history[channel] += reply + "\n"
 
     count_since_response = 0
-    last_respone_time = int(time.time())
+    last_response_time = int(time.time())
 
 
 def should_respond(message):
@@ -49,7 +55,7 @@ def should_respond(message):
             # too few messages
             return False
 
-        time_since_last_response = int(time.time()) - last_respone_time
+        time_since_last_response = int(time.time()) - last_response_time
         if time_since_last_response < config.get('respond_without_prompt', 'seconds_since_last_response'):
             # last response too recent
             return False
@@ -106,8 +112,38 @@ def admin_commands(username, channel, message, full_user):
 
     elif command in ["reload_config", "config", "reloadconfig"]:
         config.load_from_file()
-        irc.send_to_channel(channel, username + ": reloaded config.json. new command_key = " + config.get('command_key'))
+        irc.send_to_channel(channel, username + ": reloaded config.json")
+        global random_message_thread
+        if random_message_thread and not random_message_thread.is_alive():
+            random_message_thread = threading.Thread(target=try_random_message)
+            random_message_thread.start()
 
+
+def try_random_message():
+    global last_response_time, count_since_response
+    if not config.get('message_randomly_time'):
+        return
+
+    while True:
+        time.sleep(config.get('message_randomly_time'))
+        time_since_last_response = int(time.time()) - last_response_time
+        if time_since_last_response < config.get('message_randomly_time'):
+            # last message was too recent
+            continue
+
+        for channel in config.get('irc', 'channels'):
+            if channel not in message_history or len(message_history[channel]) < 10:
+                continue
+            message = chat_generator.generate_reply(message_history[channel])
+            irc.send_to_channel(channel, message)
+            message_history[channel] += message + "\n"
+            message_history[channel] = message_history[channel][-100:]
+
+        count_since_response = 0
+        last_response_time = int(time.time())
 
 irc.add_message_handler(message_handler)
 irc.add_message_handler(admin_commands)
+
+random_message_thread = threading.Thread(target=try_random_message)
+random_message_thread.start()
